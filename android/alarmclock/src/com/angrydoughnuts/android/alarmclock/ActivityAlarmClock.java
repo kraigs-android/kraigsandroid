75f9b118265872f7809d4fa3b4f264c40cb51447
kraigs.android <kraigs.android@81ac7b4a-400a-11de-9cfd-b5a970547080>
2010-03-04 21:17:16 +0000

    Move binding back to pause/resume.
    git-svn-id: https://kraigsandroid.googlecode.com/svn/trunk@163 81ac7b4a-400a-11de-9cfd-b5a970547080

--- android/alarmclock/src/com/angrydoughnuts/android/alarmclock/ActivityAlarmClock.java
+++ android/alarmclock/src/com/angrydoughnuts/android/alarmclock/ActivityAlarmClock.java
@@ -116,14 +116,9 @@ public class ActivityAlarmClock extends Activity {
   }
 
   @Override
-  protected void onStart() {
-    super.onStart();
-    service.bind();
-  }
-
-  @Override
   protected void onResume() {
     super.onResume();
+    service.bind();
     handler.post(tickCallback);
     adapter.requery();
   }
@@ -132,11 +127,6 @@ public class ActivityAlarmClock extends Activity {
   protected void onPause() {
     super.onPause();
     handler.removeCallbacks(tickCallback);
-  }
-
-  @Override
-  protected void onStop() {
-    super.onStop();
     service.unbind();
   }
 
