032ea4ce1b86d8fcb98a503e7dc039914cf628f5
kraigs.android <kraigs.android@81ac7b4a-400a-11de-9cfd-b5a970547080>
2010-03-04 20:18:28 +0000

    First attempt at alarm clock Activity/Service pair.
    git-svn-id: https://kraigsandroid.googlecode.com/svn/trunk@25 81ac7b4a-400a-11de-9cfd-b5a970547080

---
+++ android/alarmclock/src/com/angrydoughnuts/android/alarmclock/AlarmClockActivity.java
@@ -0,0 +1,79 @@
+package com.angrydoughnuts.android.alarmclock;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
+
+public class AlarmClockActivity extends Activity {
+  // TODO(cgallek): replace this with a data provider.
+  private static boolean alarmOn = false;
+  public static boolean getAlarmOn() { return alarmOn; }
+  private Intent serviceIntent;
+  private ServiceConnection serviceConnection;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    serviceIntent = new Intent(getApplication(), AlarmClockService.class);
+
+    Button startBtn = (Button) findViewById(R.id.start_service);
+    Button stopBtn = (Button) findViewById(R.id.stop_service);
+    Button setBtn = (Button) findViewById(R.id.set_alarm);
+    Button clearBtn = (Button) findViewById(R.id.clear_alarm);
+
+    startBtn.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View view) {
+        startService(serviceIntent);    
+      }
+    });
+    stopBtn.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View view) {
+        stopService(serviceIntent);
+      }
+    });
+    setBtn.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View view) {
+        alarmOn = true;
+      }
+    });
+    clearBtn.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View view) {
+        alarmOn = false;
+      }
+    });
+  }
+
+  // TODO(cgallek): should these be onStart/onStop or onResume/onPause??
+  @Override
+  protected void onStart() {
+    super.onStart();
+    serviceConnection = new ServiceConnection() {
+      @Override
+      public void onServiceConnected(ComponentName name, IBinder service) {
+      }
+      @Override
+      public void onServiceDisconnected(ComponentName name) {
+      }
+    };
+
+    boolean bindSuccess = bindService(
+        serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+    assert(bindSuccess);
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    // TODO(cgallek): This doesn't seem to call onUnbind() in the service all
+    // the time. I'm not sure why...  The services, therefore, doesn't always
+    // auto shutdown.
+    unbindService(serviceConnection);
+  }
+}
---
+++ android/alarmclock/src/com/angrydoughnuts/android/alarmclock/AlarmClockService.java
@@ -0,0 +1,66 @@
+  package com.angrydoughnuts.android.alarmclock;
+  
+  import android.app.Notification;
+  import android.app.NotificationManager;
+import android.app.PendingIntent;
+  import android.app.Service;
+  import android.content.Context;
+  import android.content.Intent;
+  import android.os.IBinder;
+import android.widget.Toast;
+  
+  public class AlarmClockService extends Service {
+    private final int NOTIFICATION_ID = 1;
+  
+    @Override
+    public void onStart(Intent intent, int startId) {
+      super.onStart(intent, startId);
+    }
+  
+    @Override
+    public void onCreate() {
+      super.onCreate();
+
+      Toast.makeText(this, "Alarm Clock Service Start", Toast.LENGTH_SHORT).show();
+      NotificationManager manager =
+        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+      Notification notification = new Notification(
+          R.drawable.icon, "Alarm Clock Service", System.currentTimeMillis());
+      Intent notificationIntent = new Intent(this, AlarmClockActivity.class);
+      PendingIntent launch = PendingIntent.getActivity(
+          this, 0, notificationIntent, 0);
+      notification.setLatestEventInfo(
+          getApplicationContext(), "title", "text", launch);
+      manager.notify(NOTIFICATION_ID, notification);
+    }
+  
+    @Override
+    public void onDestroy() {
+      super.onDestroy();
+      Toast.makeText(this, "Alarm Clock Service Stop", Toast.LENGTH_SHORT).show();
+      NotificationManager manager =
+        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+      manager.cancel(NOTIFICATION_ID);
+    }
+  
+    @Override
+    public IBinder onBind(Intent intent) {
+      // Explicitly start this service when a client binds.  This will cause
+      // the service to outlive all of its binders.
+      Intent selfIntent = new Intent(getApplication(), AlarmClockService.class);
+      startService(selfIntent);
+      return null;
+    }
+  
+    @Override
+    public boolean onUnbind(Intent intent) {
+      // Decide if we need to explicitly shut down this service.  Normally,
+      // the service would shutdown after the last un-bind.  However, since
+      // we explicitly started the service in onBind (to remain persistent),
+      // we must explicitly stop here when there are no alarms set.
+      if (!AlarmClockActivity.getAlarmOn()) {
+        stopSelf();
+      }
+      return super.onUnbind(intent);
+    }
+  }
