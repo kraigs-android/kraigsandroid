fe6a6cf8b0ce4371dea22c3de6b0876136d0040b
kraigs.android <kraigs.android@81ac7b4a-400a-11de-9cfd-b5a970547080>
2010-03-04 20:39:30 +0000

    ListView for displaying alarm DB state.
    git-svn-id: https://kraigsandroid.googlecode.com/svn/trunk@52 81ac7b4a-400a-11de-9cfd-b5a970547080

--- android/alarmclock/src/com/angrydoughnuts/android/alarmclock/AlarmClockActivity.java
+++ android/alarmclock/src/com/angrydoughnuts/android/alarmclock/AlarmClockActivity.java
@@ -1,12 +1,17 @@
 package com.angrydoughnuts.android.alarmclock;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class AlarmClockActivity extends Activity {
   private AlarmClockServiceBinder service;
+  private DbAccessor db;
+  private Cursor alarmListCursor;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,9 @@ public class AlarmClockActivity extends Activity {
     setContentView(R.layout.main);
 
     service = AlarmClockServiceBinder.newBinderAndStart(getApplicationContext());
+    db = new DbAccessor(getApplicationContext());
+    alarmListCursor = db.getAlarmList();
+    startManagingCursor(alarmListCursor);
 
     Button setBtn = (Button) findViewById(R.id.set_alarm);
     Button clearBtn = (Button) findViewById(R.id.clear_alarm);
@@ -21,13 +29,21 @@ public class AlarmClockActivity extends Activity {
     setBtn.setOnClickListener(new View.OnClickListener() {
       public void onClick(View view) {
         service.scheduleAlarmIn(5);
+        alarmListCursor.requery();
       }
     });
     clearBtn.setOnClickListener(new View.OnClickListener() {
       public void onClick(View view) {
         service.clearAllAlarms();
+        alarmListCursor.requery();
       }
     });
+
+    String[] from = new String[] { DbHelper.ALARMS_COL_ID, DbHelper.ALARMS_COL_TIME, DbHelper.ALARMS_COL_ENABLED };
+    int[] to = new int[] { R.id.alarm_id, R.id.alarm_time, R.id.alarm_enabled };
+    SimpleCursorAdapter adapter = new SimpleCursorAdapter(getApplicationContext(), R.layout.alarm_description, alarmListCursor, from, to);
+    ListView alarmList = (ListView) findViewById(R.id.alarm_list);
+    alarmList.setAdapter(adapter);
   }
 
   @Override
@@ -41,5 +57,11 @@ public class AlarmClockActivity extends Activity {
     super.onPause();
     service.unbind();
     finish();
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    db.closeConnections();
   }
 }
