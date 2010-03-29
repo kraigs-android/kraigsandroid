dad7c36f4197ff7c041946c1209bed052e66f553
kraigs.android <kraigs.android@81ac7b4a-400a-11de-9cfd-b5a970547080>
2010-03-29 01:01:21 +0000

    Re-schedule alarms when package is re-installed.
    git-svn-id: https://kraigsandroid.googlecode.com/svn/trunk@283 81ac7b4a-400a-11de-9cfd-b5a970547080

--- android/alarmclock/src/com/angrydoughnuts/android/alarmclock/ReceiverDeviceBoot.java
+++ android/alarmclock/src/com/angrydoughnuts/android/alarmclock/ReceiverDeviceBoot.java
@@ -8,6 +8,16 @@ public class ReceiverDeviceBoot extends BroadcastReceiver {
 
   @Override
   public void onReceive(Context context, Intent intent) {
+    // There doesn't seem to be any way to filter on the scheme-specific
+    // portion of the ACTION_PACKANGE_REPLACED intent (the package
+    // being replaced is in the ssp).  Since we can't filter for it in the
+    // Manifest file, we get every package replaced event and cancel this
+    // event if it's not our package.
+    if (intent.getAction().equals(Intent.ACTION_PACKAGE_REPLACED)) {
+      if (!intent.getData().getSchemeSpecificPart().equals(context.getPackageName())) {
+        return;
+      }
+    }
     Intent i = new Intent(context, AlarmClockService.class);
     i.putExtra(AlarmClockService.COMMAND_EXTRA, AlarmClockService.COMMAND_DEVICE_BOOT);
     context.startService(i);
