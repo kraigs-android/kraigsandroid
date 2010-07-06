91de9d667a90bcf29071768b27a593039e75cba2
kraigs.android <kraigs.android@81ac7b4a-400a-11de-9cfd-b5a970547080>
2010-07-06 16:50:12 +0000

    Add motion tracking to new slider.
    git-svn-id: https://kraigsandroid.googlecode.com/svn/trunk@330 81ac7b4a-400a-11de-9cfd-b5a970547080

--- android/alarmclock/src/com/angrydoughnuts/android/alarmclock/Slider2.java
+++ android/alarmclock/src/com/angrydoughnuts/android/alarmclock/Slider2.java
@@ -2,6 +2,7 @@ package com.angrydoughnuts.android.alarmclock;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -10,6 +11,7 @@ import android.widget.ImageView.ScaleType;
 public class Slider2 extends ViewGroup {
   private ImageView dot;
   private ImageView target;
+  private boolean tracking;
 
   public Slider2(Context context) {
     this(context, null, 0);
@@ -33,6 +35,8 @@ public class Slider2 extends ViewGroup {
     target.setScaleType(ScaleType.CENTER);
     target.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
     addView(target);
+
+    tracking = false;
   }
 
   @Override
@@ -58,5 +62,58 @@ public class Slider2 extends ViewGroup {
         Math.max(MeasureSpec.getSize(widthMeasureSpec),
             dot.getMeasuredWidth() + target.getMeasuredWidth()),
         Math.max(dot.getMeasuredHeight(), target.getMeasuredHeight()));
+  }
+
+  // TODO(cgallek): Add some wiggle room to these.
+  private boolean withinX(View v, float x) {
+    if (x < v.getLeft() || x > v.getRight()) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  private boolean withinY(View v, float y) {
+    if (y < v.getTop() || y > v.getBottom()) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+    final int action = event.getAction();
+    final float x = event.getX();
+    final float y = event.getY();
+    switch (action) {
+      case MotionEvent.ACTION_DOWN:
+        tracking = withinX(dot, x) && withinY(dot, y);
+        return tracking || super.onTouchEvent(event);
+      case MotionEvent.ACTION_MOVE:
+        if (!tracking) {
+          return super.onTouchEvent(event);
+        }
+        if (withinY(dot, y)) {
+          dot.offsetLeftAndRight((int) (x - dot.getLeft() - dot.getWidth()/2 ));
+        } else {
+          tracking = false;
+          dot.offsetLeftAndRight(getLeft() - dot.getLeft());
+        }
+        invalidate();
+        return true;
+      case MotionEvent.ACTION_CANCEL:
+      case MotionEvent.ACTION_UP:
+        if (!tracking) {
+          return super.onTouchEvent(event);
+        }
+        tracking = false;
+        dot.offsetLeftAndRight(getLeft() - dot.getLeft());
+        invalidate();
+        return true;
+      default:
+        return super.onTouchEvent(event);
+    }
   }
 }
