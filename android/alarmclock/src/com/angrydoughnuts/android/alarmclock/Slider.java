638d5b7caf0f6b2aae4d52477d6f0daac26e1d4e
kraigs.android <kraigs.android@81ac7b4a-400a-11de-9cfd-b5a970547080>
2010-07-06 16:51:27 +0000

    Slider comments.
    git-svn-id: https://kraigsandroid.googlecode.com/svn/trunk@343 81ac7b4a-400a-11de-9cfd-b5a970547080

--- android/alarmclock/src/com/angrydoughnuts/android/alarmclock/Slider.java
+++ android/alarmclock/src/com/angrydoughnuts/android/alarmclock/Slider.java
@@ -1,3 +1,18 @@
+/****************************************************************************
+ * Copyright 2010 kraigs.android@gmail.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+
 package com.angrydoughnuts.android.alarmclock;
 
 import android.content.Context;
@@ -15,7 +30,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.ImageView.ScaleType;
 
-// TODO comment/cleanup this file.
+/**
+ * Widget that contains a slider bar used for acknowledgments.  The user
+ * must slide an arrow sufficiently far enough across the bar in order
+ * to trigger the acknowledgment.
+ */
 public class Slider extends ViewGroup {
   public interface OnCompleteListener {
     void complete();
@@ -41,6 +60,7 @@ public class Slider extends ViewGroup {
 
   public Slider(Context context, AttributeSet attrs, int defStyle) {
     super(context, attrs, defStyle);
+    // Setup the background which 'holds' the slider.
     tray = new TextView(getContext());
     tray.setBackgroundResource(R.drawable.slider_background);
     tray.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
@@ -49,6 +69,7 @@ public class Slider extends ViewGroup {
     tray.setText(R.string.dismiss);
     addView(tray);
 
+    // Setup the object which will be slid.
     dot = new ImageView(getContext());
     dot.setImageResource(R.drawable.slider_icon);
     dot.setBackgroundResource(R.drawable.slider_btn);
@@ -98,7 +119,6 @@ public class Slider extends ViewGroup {
         Math.max(tray.getMeasuredHeight(), dot.getMeasuredHeight()));
   }
 
-  // TODO(cgallek): Add some wiggle room to these.
   private boolean withinX(View v, float x) {
     if (x < v.getLeft() || x > v.getRight()) {
       return false;
