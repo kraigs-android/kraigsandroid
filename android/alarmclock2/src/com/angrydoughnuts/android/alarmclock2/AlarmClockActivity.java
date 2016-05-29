63302582dc749eec6f98559f32b309ae26bee266
Craig Gallek <cgallek@angrydoughnuts.com>
2016-05-29 18:22:52 -0400

    first attempt at keyboard input for time picker

--- android/alarmclock2/src/com/angrydoughnuts/android/alarmclock2/AlarmClockActivity.java
+++ android/alarmclock2/src/com/angrydoughnuts/android/alarmclock2/AlarmClockActivity.java
@@ -32,6 +32,9 @@ import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -41,6 +44,8 @@ import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class AlarmClockActivity extends Activity {
   private AlarmClockService service = null;
@@ -148,35 +153,74 @@ public class AlarmClockActivity extends Activity {
         new View.OnClickListener() {
           @Override
           public void onClick(View view) {
+            // TODO: input values
             Bundle b = new Bundle();
-            b.putLong("time", System.currentTimeMillis());
+            //b.putLong("time", System.currentTimeMillis());
 
             DialogFragment f = new DialogFragment() {
-              @Override
-              public Dialog onCreateDialog(Bundle savedInstanceState) {
-                final View v = getLayoutInflater().inflate(R.layout.time_picker, null);
-                ((EditText)v.findViewById(R.id.time_entry)).setText("" + getArguments().getLong("time"));
-
-                return new AlertDialog.Builder(getContext())
-                  .setTitle("New Alarm")
-                  //.setIcon()
-                  .setView(v)
-                  .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public Dialog onCreateDialog(Bundle savedInstanceState) {
+                  final Calendar c = Calendar.getInstance();
+                  final View v = getLayoutInflater().inflate(R.layout.time_picker, null);
+                  // TODO: input values
+                  //getArguments().getLong("time");
+                  // TODO: HOUR/HOUR_OF_DAY for AM/PM??
+                  final EditText e = (EditText)v.findViewById(R.id.time_entry);
+                  e.setText(String.format("%02d:%02d", c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE)));
+                  e.addTextChangedListener(new TextWatcher() {
                       @Override
-                      public void onClick(DialogInterface dialog, int which) {}
-                    })
-                  .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                      public void beforeTextChanged(CharSequence s, int st, int c, int a) {}
                       @Override
-                      public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(
-                            getContext(),
-                            ((EditText)v.findViewById(R.id.time_entry)).getText(),
-                            Toast.LENGTH_SHORT).show();
+                      public void onTextChanged(CharSequence s, int st, int b, int c) {}
+                      @Override
+                      public void afterTextChanged(Editable s) {
+                        String hhmm = s.toString().replaceAll(":", "");
+                        if (hhmm.isEmpty()) {
+                          c.set(Calendar.HOUR_OF_DAY, 0);
+                          c.set(Calendar.MINUTE, 0);
+                        } else if (hhmm.length() <= 2) {
+                          c.set(Calendar.HOUR_OF_DAY, 0);
+                          c.set(Calendar.MINUTE, Integer.parseInt(hhmm));
+                        } else {
+                          c.set(Calendar.HOUR_OF_DAY, Integer.parseInt(hhmm.substring(0, hhmm.length() - 2)));
+                          c.set(Calendar.MINUTE, Integer.parseInt(hhmm.substring(hhmm.length() - 2, hhmm.length())));
+                        }
+                        e.removeTextChangedListener(this);
+                        e.setText(String.format("%02d:%02d", c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE)));
+                        e.setSelection(e.getText().length());
+                        e.addTextChangedListener(this);
                       }
+                    });
+
+                  final AlertDialog d = new AlertDialog.Builder(getContext())
+                    .setTitle("New Alarm")
+                    //.setIcon()
+                    .setView(v)
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {}
                     })
-                  .create();
-              }
-            };
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                          Toast.makeText(
+                              getContext(),
+                              String.format("%02d:%02d", c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE)),
+                              Toast.LENGTH_SHORT).show();
+                        }
+                      })
+                    .create();
+                  e.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                      @Override
+                      public boolean onEditorAction(TextView v, int action, KeyEvent e) {
+                        d.getButton(DialogInterface.BUTTON_POSITIVE).performClick();
+                        return true;
+                      }
+                    });
+
+                  return d;
+                }
+              };
 
             f.setArguments(b);
             f.show(getFragmentManager(), "new_alarm");
