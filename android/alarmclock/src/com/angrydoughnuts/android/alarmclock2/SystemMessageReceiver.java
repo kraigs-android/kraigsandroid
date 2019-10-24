ded03e423aeb978bc3a795d227db30d1b6d819d5
Craig Gallek <cgallek@angrydoughnuts.com>
2019-10-23 21:59:04 -0400

    android Q cant start services from receivers

--- android/alarmclock/src/com/angrydoughnuts/android/alarmclock2/SystemMessageReceiver.java
+++ android/alarmclock/src/com/angrydoughnuts/android/alarmclock2/SystemMessageReceiver.java
@@ -15,12 +15,18 @@
 
 package com.angrydoughnuts.android.alarmclock;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.Build;
+import android.os.IBinder;
 import android.util.Log;
 
 import java.util.LinkedList;
@@ -29,39 +35,28 @@ import java.util.List;
 public class SystemMessageReceiver extends BroadcastReceiver {
   @Override
   public void onReceive(Context c, Intent i) {
+    int command;
     if (i.getAction().equals(Intent.ACTION_TIMEZONE_CHANGED)) {
       Log.i(TAG, "Timezone change...");
-      rescheduleAll(c);
+      command = RescheduleService.RESCHEDULE;
     } else if (i.getAction().equals(Intent.ACTION_MY_PACKAGE_REPLACED)) {
       Log.i(TAG, "Installed...");
-      scheduleAll(c);
+      command = RescheduleService.SCHEDULE;
     } else if (i.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
       Log.i(TAG, "Boot complete...");
-      scheduleAll(c);
+      command = RescheduleService.SCHEDULE;
+    } else {
+      Log.i(TAG, "Unknown broadcast" + i.toString());
+      return;
     }
-  }
 
-  private void clearSnooze(Context c) {
-    ContentValues val = new ContentValues();
-    val.put(AlarmClockProvider.AlarmEntry.NEXT_SNOOZE, 0);
-    c.getContentResolver().update(
-        AlarmClockProvider.ALARMS_URI, val, null, null);
-  }
+    Intent reschedule = new Intent(c, RescheduleService.class)
+      .putExtra(RescheduleService.COMMAND, command);
 
-  private void rescheduleAll(Context c) {
-    clearSnooze(c);
-    for (Alarm a : enabledAlarms(c.getContentResolver())) {
-      Log.i(TAG, "Rescheduling alarm " + a.id);
-      AlarmNotificationService.removeAlarmTrigger(c, a.id);
-      AlarmNotificationService.scheduleAlarmTrigger(c, a.id, a.tsUTC);
-    }
-  }
-
-  private void scheduleAll(Context c) {
-    clearSnooze(c);
-    for (Alarm a : enabledAlarms(c.getContentResolver())) {
-      Log.i(TAG, "Scheduling alarm " + a.id);
-      AlarmNotificationService.scheduleAlarmTrigger(c, a.id, a.tsUTC);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      c.startForegroundService(reschedule);
+    } else {
+      c.startService(reschedule);
     }
   }
 
@@ -78,21 +73,83 @@ public class SystemMessageReceiver extends BroadcastReceiver {
     }
   }
 
-  private List<Alarm> enabledAlarms(ContentResolver r) {
-    LinkedList<Alarm> ids = new LinkedList<Alarm>();
-    Cursor c = r.query(
-        AlarmClockProvider.ALARMS_URI,
-        new String[] {
-          AlarmClockProvider.AlarmEntry._ID,
-          AlarmClockProvider.AlarmEntry.TIME,
-          AlarmClockProvider.AlarmEntry.DAY_OF_WEEK,
-          AlarmClockProvider.AlarmEntry.NEXT_SNOOZE },
-        AlarmClockProvider.AlarmEntry.ENABLED + " == 1",
-        null, null);
-    while (c.moveToNext())
-      ids.add(new Alarm(c));
-    c.close();
-    return ids;
+  // Hacky foreground service that delegates to the original (background)
+  // service.  Newer Androids can't start background services from
+  // BroadcastReceivers.
+  public static class RescheduleService extends Service {
+    public static final String COMMAND = "command";
+    public static final int RESCHEDULE = 1;
+    public static final int SCHEDULE = 2;
+
+    @Override
+    public IBinder onBind(Intent intent) { return null; }
+    @Override
+    public int onStartCommand(Intent i, int flags, int startId) {
+      final String CHAN = "reschedule";
+      final NotificationManager manager =
+        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O &&
+          manager.getNotificationChannel(CHAN) == null) {
+        manager.createNotificationChannel(
+            new NotificationChannel(
+                CHAN, CHAN, NotificationManager.IMPORTANCE_LOW)); // No sound.
+      }
+      final Notification notification =
+        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ?
+         new Notification.Builder(this, CHAN) :
+         new Notification.Builder(this))
+        .setSmallIcon(R.drawable.ic_alarm_on)
+        .setCategory(Notification.CATEGORY_ALARM)
+        .setPriority(Notification.PRIORITY_MIN)
+        .setVisibility(Notification.VISIBILITY_PUBLIC)
+        .build();
+      startForeground(6969, notification);
+
+      switch (i.getExtras().getInt(COMMAND)) {
+      case RESCHEDULE:
+        clearSnooze(this);
+        for (Alarm a : enabledAlarms(getContentResolver())) {
+          Log.i(TAG, "Rescheduling alarm " + a.id);
+          AlarmNotificationService.removeAlarmTrigger(this, a.id);
+          AlarmNotificationService.scheduleAlarmTrigger(this, a.id, a.tsUTC);
+        }
+        break;
+      case SCHEDULE:
+        clearSnooze(this);
+        for (Alarm a : enabledAlarms(getContentResolver())) {
+          Log.i(TAG, "Scheduling alarm " + a.id);
+          AlarmNotificationService.scheduleAlarmTrigger(this, a.id, a.tsUTC);
+    }
+        break;
+      }
+
+      stopSelf();
+      return START_NOT_STICKY;
+    }
+
+    private void clearSnooze(Context c) {
+      ContentValues val = new ContentValues();
+      val.put(AlarmClockProvider.AlarmEntry.NEXT_SNOOZE, 0);
+      c.getContentResolver().update(
+          AlarmClockProvider.ALARMS_URI, val, null, null);
+    }
+
+    private List<Alarm> enabledAlarms(ContentResolver r) {
+      LinkedList<Alarm> ids = new LinkedList<Alarm>();
+      Cursor c = r.query(
+          AlarmClockProvider.ALARMS_URI,
+          new String[] {
+            AlarmClockProvider.AlarmEntry._ID,
+            AlarmClockProvider.AlarmEntry.TIME,
+            AlarmClockProvider.AlarmEntry.DAY_OF_WEEK,
+            AlarmClockProvider.AlarmEntry.NEXT_SNOOZE },
+          AlarmClockProvider.AlarmEntry.ENABLED + " == 1",
+          null, null);
+      while (c.moveToNext())
+        ids.add(new Alarm(c));
+      c.close();
+      return ids;
+    }
   }
 
   private static final String TAG = SystemMessageReceiver.class.getSimpleName();
